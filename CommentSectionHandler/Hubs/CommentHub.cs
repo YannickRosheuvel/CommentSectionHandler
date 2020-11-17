37dc9969be94324aac52b4989811b43eceb4cd49
Yannick <yannick.rosheuvel@gmail.com>
2020-11-17 11:20:17 +0100

    init commit

---
+++ CommentSectionHandler/Hubs/CommentHub.cs
@@ -0,0 +1,14 @@
+﻿using CommentSectionHandler.Interfaces;
+using CommentSectionHandler.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSystem.Hubs
+{
+    public class CommentHub : Hub
+    {
+    }
+}
